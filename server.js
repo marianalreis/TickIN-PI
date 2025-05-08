aafe79f29618504f0e3a292f9fbb7d58a36fa49c
Mariana Lacerda Reis <mariana.reis@sou.inteli.edu.br>
2025-05-08 00:21:48 -0300

    feat: adicionar mvc, server.js e modelagem do banco de dados

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); // permite receber dados no formato JSON
+
+const routes = require('./routes/index');
+app.use('/', routes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
