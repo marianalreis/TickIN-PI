88eba495408b0679e7859c40c70930e43b2caaa7
Mariana Lacerda Reis <mariana.reis@sou.inteli.edu.br>
2025-05-16 08:09:07 -0300

    fix: corrigir fidelidade de wireframes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const TarefaController = require('../controllers/TarefaController');
+
+router.post('/tarefas', TarefaController.criarTarefa);
+router.get('/tarefas', TarefaController.listarTarefas);
+router.put('/tarefas/:id', TarefaController.editarTarefa);
+router.delete('/tarefas/:id', TarefaController.excluirTarefa);
+
+module.exports = router;
